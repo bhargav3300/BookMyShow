e12c23836d466f0a4d79b6d969e9dda39e0af137
bhargav3300 <85093219+bhargav3300@users.noreply.github.com>
2025-09-18 10:36:22 -0400

    Update Entertainmentcard.component.js

--- src/components/Entertainment/Entertainmentcard.component.js
+++ src/components/Entertainment/Entertainmentcard.component.js
@@ -15,13 +15,13 @@ const EntertainmentCard = (props) => {
 
 const EntertainmentCardSlider = () => {
     const EntertainmentImage = [
-      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-ODUrIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/workshops-collection-202007231330.png",
-      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MSBFdmVudA%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/fitness-collection-2020081150.png",
-      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/kids-collection-202007220710.png",
-      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MjArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/comedy-shows-collection-202007220710.png",
-      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-OCBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/music-shows-collection-202007220710.png",
-      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTUrIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/esports-collection-202011150107.png",
-      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-OSBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/self-improvement-collection-202007220710.png",
+      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800,w-300/workshops-collection-202007231330.png",
+      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800,w-300/fitness-collection-2020081150.png",
+      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800,w-300/kids-collection-202007220710.png",
+      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800,w-300/comedy-shows-collection-202007220710.png",
+      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800,w-300/music-shows-collection-202007220710.png",
+      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800,w-300/esports-collection-202011150107.png",
+      "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800,w-300/self-improvement-collection-202007220710.png",
       "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:w-300/cooking-collection-202007222211.png",
       "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:w-300/interactive-games-collection-202012041129.png"
     ];
